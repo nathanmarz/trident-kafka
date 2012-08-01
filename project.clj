405aa4013f5fb635ca158116561dfab211b39c6b
Nathan Marz <nathan@twitter.com>
2012-08-01 16:43:43 -0700

    clean up project.clj

--- project.clj
+++ project.clj
@@ -2,8 +2,6 @@
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
-  :repositories {"releases" "http://artifactory.local.twitter.com/libs-releases-local"
-                 "snapshots" "http://artifactory.local.twitter.com/libs-snapshots-local"}
   :dependencies [[storm/kafka "0.7.0-incubating"
                    :exclusions [org.apache.zookeeper/zookeeper
                                 log4j/log4j]]]
