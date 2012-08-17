82027211fa58654243204bed0b7b22c52d8e1e1b
Nathan Marz <nathan@twitter.com>
2012-08-17 14:03:52 -0700

    scala 2.9.2 version

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject storm/trident-kafka "0.0.2-wip1"
+(defproject storm/trident-kafka "0.0.2-scala-2.9.2-wip1"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
-  :dependencies [[storm/kafka "0.7.0-incubating"
+  :repositories {"conjars" "http://conjars.org/repo"}
+  :dependencies [[org.scala-lang/scala-library "2.9.2"]
+                 [com.twitter/kafka_2.9.2 "0.7.0"
                    :exclusions [org.apache.zookeeper/zookeeper
                                 log4j/log4j]]]
   :dev-dependencies [[storm "0.8.0"]
