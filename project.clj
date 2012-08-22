aa4bd119d61e1f44cc063b995c015c0fc3f26ba8
Nathan Marz <nathan@twitter.com>
2012-08-22 01:16:02 -0700

    fix bug where force offset was not being used if there was no metadata for the partition

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject storm/trident-kafka "0.0.2-wip2"
+(defproject storm/trident-kafka "0.0.2-wip3"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
